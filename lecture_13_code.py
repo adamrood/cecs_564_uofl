1efa281b66d809653e7164dabbacff471212fe34
Adam <39994578+adamrood@users.noreply.github.com>
2020-02-24 12:27:53 -0500

    Add files via upload

---
+++ lecture_13_code.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+p = [.25, .30, .15, .30]
+pk = [.25, .50, .25]
+e = [[3, 4, 2, 1],[3, 1, 4, 2],[4, 3, 1, 2]]
+
+def Bayes(p, pk, e):
+    ppgivenc = []
+    m = len(pk)
+    n = len(p)
+    q = np.zeros(n)
+    pcgivenp = np.zeros((n,n))
+    ppgivenc = np.zeros((n,n))
+    print(ppgivenc)
+    for i in range(m):
+        for j in range(n):
+            q[e[i][j] - 1] = q[e[i][j] - 1] + pk[i] * p[j]
+            pcgivenp[e[i][j] - 1][j] = pcgivenp[e[i][j] - 1][j] + pk[i];
+    for i in range(n):
+        for j in range(n):
+            ppgivenc[i][j] = pcgivenp[j][i] * p[i]/q[j]
+    return q, ppgivenc
+
+Bayes(p,pk,e)
